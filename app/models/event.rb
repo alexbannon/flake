f69059c252ef2ea6abef7d3a463ed3b16be098ba
Alex Bannon <alexbannon@gmail.com>
2015-08-05 23:35:01 -0400

    refactoring and partials

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, dependent: :destroy
   has_many :attendances, dependent: :destroy
   has_many :users, through: :attendances
 end
