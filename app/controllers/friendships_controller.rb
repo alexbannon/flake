409a3612884b17462ad46488e9c8a0f474dff184
Alex Bannon <alexbannon@gmail.com>
2015-08-06 20:18:09 -0400

    destroy friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,7 +4,6 @@ class FriendshipsController < ApplicationController
     user_helper
     @friendship = @user.friendships.build(:friend_id => params[:friend_id])
     if @friendship.save
-      flash[:message] = "Added friend"
       redirect_to root_url
     else
       flash[:message] = "Error adding friend"
@@ -13,7 +12,8 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    @friendship = Friendship.find(params[:id])
+    user_helper
+    @friendship = @user.friendships.find(:friend_id => params[:id])
     @friendship.destroy
     flash[:message] = "Successfully destroyed friendship"
     redirect_to root_url
